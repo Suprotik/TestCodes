f63c65b36a466f36c1ed0bfe36751b5cc0ebef7c
SUBHO <SUBHO@SUBHO-PC>
2019-03-10 23:32:26 +0530

    GUIFXproject1

---
+++ JavaProjects/src/FXGUICODES/GUITEST1.java
@@ -0,0 +1,20 @@
+package FXGUICODES;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class GUITEST1 extends Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		primaryStage.setTitle("My First JavaFX App");
+        primaryStage.show();
+	}
+
+}
