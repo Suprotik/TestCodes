6ecc97554e418dced2d9230d955f8ee1bd532baf
SUBHO <SUBHO@SUBHO-PC>
2019-03-12 21:48:25 +0530

    test purpose only

--- JavaProjects/src/FXGUICODES/GUITEST1.java
+++ JavaProjects/src/FXGUICODES/GUITEST1.java
@@ -1,13 +1,14 @@
 package FXGUICODES;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.TilePane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -21,7 +22,7 @@ public class GUITEST1 extends Application {
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		// TODO Auto-generated method stub
-		TilePane rootPane = new TilePane();
+		BorderPane rootPane = new BorderPane();
 		Scene scene = new Scene(rootPane,800,400);
 		
 		
@@ -31,22 +32,38 @@ public class GUITEST1 extends Application {
 		MenuItem MenuItem1 = new MenuItem("New"); //creating Menu Item 
 		MenuName1.getItems().addAll(MenuItem1,new MenuItem("Save")); //adding Menu Item to the Menu  
 		menubar.getMenus().addAll(MenuName1,MenuName2); //adding Menu to the MenuBar 
-		VBox root1 = new VBox(menubar);
-		root1.setMinSize(800, 20);
+		rootPane.setTop(menubar);
+		
+		//VBox root1 = new VBox(menubar);
+		//root1.setMinSize(800, 20);
 		//root1.getChildren().add(menubar);
 
-		GridPane root2=new GridPane();
+		/*GridPane root2=new GridPane();
 		Button btn1=new Button("button 1"); 
-		root2.getChildren().add(btn1);
+		//root2.getChildren().add(btn1);
 		Button btn2=new Button("button 2"); 
-		root2.getChildren().add(btn2);
+		//root2.getChildren().add(btn2);
 		Button btn3=new Button("button 3"); 
-		root2.getChildren().add(btn3);
+		//root2.getChildren().add(btn3);
+		//root2.addRow(0, btn1,btn2,btn3);
+		root2.add(btn1, 0, 0, 1, 1);;
+		root2.add(btn2, 3, 3, 1, 1);;
+		root2.setHgap(20);
+		root2.setVgap(20);
+		root2.setAlignment(Pos.TOP_LEFT);*/
+		Button btn1=new Button("button 1");
+		rootPane.setCenter(btn1);
+		Button btn2=new Button("button 2"); 
+		rootPane.setLeft(btn2);
+		Button btn3=new Button("button 3"); 
+		rootPane.setRight(btn3);
+
+
 		
 		
 		
 		
-		rootPane.getChildren().addAll(root1,root2);
+		//rootPane.getChildren().addAll(root1,root2);
 		primaryStage.setTitle("My First JavaFX App");
 		primaryStage.setScene(scene);
         primaryStage.show();
