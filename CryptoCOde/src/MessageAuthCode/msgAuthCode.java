d5f64e3319c565dda69cc20bbcb35487ff75c50c
Subho <Subho@Subho-PC>
2021-05-30 01:02:26 +0530

    Test

---
+++ CryptoCOde/src/MessageAuthCode/msgAuthCode.java
@@ -0,0 +1,40 @@
+package MessageAuthCode;
+
+import java.security.InvalidKeyException;
+import java.security.Key;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+
+import javax.crypto.KeyGenerator;
+import javax.crypto.Mac;
+
+public class msgAuthCode {
+
+	public static void main(String[] args) throws NoSuchAlgorithmException, InvalidKeyException {
+		// TODO Auto-generated method stub
+		//Creating a KeyGenerator object
+	      KeyGenerator keyGen = KeyGenerator.getInstance("DES");
+
+	      //Creating a SecureRandom object
+	      SecureRandom secRandom = new SecureRandom();
+
+	      //Initializing the KeyGenerator
+	      keyGen.init(secRandom);
+
+	      //Creating/Generating a key
+	      Key key = keyGen.generateKey();	 
+	      //Creating a Mac object
+	      Mac mac = Mac.getInstance("HmacSHA256");
+
+	      //Initializing the Mac object
+	      mac.init(key);
+
+	      //Computing the Mac
+	      String msg = new String("TestMAC");
+	      byte[] bytes = msg.getBytes();      
+	      byte[] macResult = mac.doFinal(bytes);
+	      System.out.println("Input :" +msg);
+	      System.out.println("Mac result:"+new String(macResult));
+	}
+
+}
